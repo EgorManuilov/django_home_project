9616151b4c6afa4bec4f32d30d1a5be5e7169adb
EgorManuilov <egor_manuilov@mail.ru>
2023-08-21 15:36:29 +0300

    hw 3

--- catalog/views.py
+++ catalog/views.py
@@ -1,15 +1,26 @@
 from django.shortcuts import render
 
+from catalog.models import Category
 
-# Create your views here.
+
 def home(request):
     return render(request, 'catalog/index.html')
 
 
 def contacts(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        phone = request.POST.get('phone')
-        message = request.POST.get('message')
-        print(name, phone, message)
-    return render(request, 'catalog/contacts.html')
+    category_list = Category.objects.all()
+    context = {
+        'object_list': category_list
+    }
+    return render(request, 'catalog/contacts.html', context)
+
+# def contacts(request):
+#     if request.method == 'POST':
+#         name = request.POST.get('name')
+#         phone = request.POST.get('phone')
+#         message = request.POST.get('message')
+#         print(name, phone, message)
+#     return render(request, 'catalog/contacts.html')
+
+
+

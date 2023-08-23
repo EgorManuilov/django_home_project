6775308e41c634881c101f62a0558b62d4a00bde
EgorManuilov <egor_manuilov@mail.ru>
2023-08-23 14:55:53 +0300

    hw 3 add

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from catalog import views
 from catalog.apps import CatalogConfig
+from catalog.views import ProductListView, ProductDetailView, contacts, product_add
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', views.home),
-    path('home/', views.home),
-    path('contacts/', views.contacts)
+    path('', ProductListView.as_view(), name='index'),
+    path('contacts/', contacts),
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='product'),
+    path('great_prod/', product_add)
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,26 +1,44 @@
 from django.shortcuts import render
+from django.utils.datetime_safe import datetime
+from django.views.generic import ListView, DetailView
 
-from catalog.models import Category
+from catalog.models import Category, Product
 
 
-def home(request):
-    return render(request, 'catalog/index.html')
+class ProductListView(ListView):
+    model = Product
+    template_name = 'catalog/index.html'
 
 
 def contacts(request):
-    category_list = Category.objects.all()
-    context = {
-        'object_list': category_list
-    }
-    return render(request, 'catalog/contacts.html', context)
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(name, phone, message)
 
-# def contacts(request):
-#     if request.method == 'POST':
-#         name = request.POST.get('name')
-#         phone = request.POST.get('phone')
-#         message = request.POST.get('message')
-#         print(name, phone, message)
-#     return render(request, 'catalog/contacts.html')
+    return render(request, 'catalog/contacts.html')
 
 
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'catalog/product.html'
 
+
+def product_add(request):
+    product_for_create = []
+    if request.method == 'POST':
+        product = {
+            'name_prod': request.POST.get('name'),
+            'description_prod': request.POST.get('description'),
+            'category_prod': request.POST.get('category'),
+            'price_prod': request.POST.get('price'),
+            'data_create_prod': datetime.now(),
+            'data_change_prod': datetime.now()
+        }
+        product_for_create.append(
+            Product(**product)
+        )
+        Product.objects.bulk_create(product_for_create)
+
+    return render(request, 'catalog/great_prod.html')

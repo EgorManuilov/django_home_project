44507c79aef0a1d46336b3c377f21056550f8d6d
EgorManuilov <egor_manuilov@mail.ru>
2023-09-16 00:09:55 +0300

    fix forms

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,17 +1,17 @@
-from django.forms import forms
+from django import forms
 
 from catalog.models import Product, Version
 
 
 class StyleFormMixin:
     def __init__(self, *args, **kwargs):
-        super.__init__(self, *args, **kwargs)
+        super().__init__(self, *args, **kwargs)
         for field_name, field in self.fields.items():
             if field_name != 'is_active':
                 field.widget.attrs['class'] = 'form-control'
 
 
-class ProductForm(StyleFormMixin, forms.ModelsForm):
+class ProductForm(forms.ModelForm, StyleFormMixin):
     class Meta:
         model = Product
         fields = "__all__"
